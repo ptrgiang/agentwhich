3f25aac9bf605261fb8ce371a367d450cfd5b3d0
Giang Pham Truong <115765258+ptrgiang@users.noreply.github.com>
2026-09-12 00:46:03 +0700

    feat: expose package API

---
+++ src/agentwhich/__init__.py
@@ -0,0 +1,7 @@
+from .compare import Comparison, compare_results
+from .core import resolve
+from .model import InstructionLayer, ResolutionResult
+
+__version__ = '0.1.0'
+
+__all__ = ['Comparison', 'InstructionLayer', 'ResolutionResult', 'compare_results', 'resolve']
